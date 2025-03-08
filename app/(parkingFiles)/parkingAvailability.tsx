68eec560e167b5890d1c40300d2abbcf026f4f2d
VictoriaRobles3 <traroblx@gmail.com>
2025-03-08 13:52:39 -0500

    Add parkingAvailability.tsx to (parkingFiles) directory. This is a sample page. Needs UI improvements.

---
+++ app/(parkingFiles)/parkingAvailability.tsx
@@ -0,0 +1,38 @@
+import { View, Text, StyleSheet } from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
+
+export default function ParkingAvailability() {
+  const { deck } = useLocalSearchParams();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Parking Availability</Text>
+      <Text style={styles.subtitle}>Selected Deck: {deck}</Text>
+      <Text style={styles.subtitle}>Parking Spots Available: </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#F8F9FA',
+  },
+  heading: {
+    fontSize: 24,
+    fontFamily: 'Montserrat-Bold',
+    color: '#0039A6',
+    marginBottom: 50,
+    textAlign: 'center',
+  },
+  subtitle: {
+    fontSize: 18,
+    fontFamily: 'Montserrat-SemiBold',
+    color: '#666',
+    marginBottom: 30,
+    textAlign: 'center',
+  },
+});
