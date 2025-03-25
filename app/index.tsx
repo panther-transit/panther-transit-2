f2cad3c1b1031b66db7b4d3f50a6d834e6ba6c5c
VictoriaRobles3 <traroblx@gmail.com>
2025-03-25 09:18:02 -0400

    npm install @react-native-async-storage/async-storage@^2.1.2

--- app/index.tsx
+++ app/index.tsx
@@ -41,7 +41,7 @@ export default function Index() {
       <View style={styles.buttonContainer}>
         <Pressable 
           style={({pressed}) => [styles.button, pressed && styles.buttonPressed]}
-          onPress={() => router.push('/(auth)/login')}
+          onPress={() => router.push('/(tabs)')}
         >
           <Text style={styles.buttonText}>Log in</Text>
         </Pressable>
